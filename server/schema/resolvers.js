6a5d3a9d91256c5381c1d042a4d0e4b3076f0b67
noah-10 <noah.fryman.10@gmail.com>
2024-04-17 17:13:44 -0400

    Added schema typeDefs and resolvers

---
+++ server/schema/resolvers.js
@@ -0,0 +1,89 @@
+// import user model
+const { User } = require('../models');
+// import sign token function for auth
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent,args, context) => {
+            if(context.user){
+                return User.findOne({ _id: context.user._id}).populate('savedBooks');
+            }else{
+                console.log('Error getting ME');
+                return;
+            }
+        },
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ $or: [{ email }, {username}] });
+
+            if(!user){
+                console.log("No account found");
+                return;
+            };
+
+            const checkPassword = await user.isCorrectPassword(password);
+
+            if(!checkPassword){
+                console.log('Password wrong');
+                return;
+            };
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({
+                username,
+                email,
+                password
+            });
+
+            if(!user){
+                console.log('Something is wrong');
+                return;
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+        saveBook: async (parent, { bookToSave }, context) => {
+            console.log(context.user);
+            try{
+                if(context.user){
+                    const savedBook = await User.findOneAndUpdate(
+                        { _id: context.user._id},
+                        { $addToSet: { savedBooks: bookToSave }},
+                        { new: true, runValidators: true }
+                    );
+                    return savedBook;
+                }    
+            }catch(err){
+                console.log(err);
+                return err
+            }
+        },
+
+        removeBook: async (parent, { book }, context) => {
+            const updatedUser = await User.findOneAndDelete(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: book }}},
+                { new: true }
+            );
+
+            if(!updatedUser){
+                return console.log('Error no user')
+            };
+
+            return updatedUser;
+        }
+    }
+}
+
+module.exports = resolvers;
