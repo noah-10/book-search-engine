e4a72d14dbb4bb1738eb85661b1d0143b8a7a539
noah-10 <noah.fryman.10@gmail.com>
2024-04-18 00:07:10 -0400

    Created Apollo provider to make every request work with the apollo server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,44 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// construct main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// request middleware that will attach the auth token to every request as an authorized header
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Sets up client to execute the authLink middleware prior to making the request to GraphQL API
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <ApolloProvider client = {client}>
+        <Navbar />
+        <Outlet />
+      </ApolloProvider>
     </>
   );
 }
